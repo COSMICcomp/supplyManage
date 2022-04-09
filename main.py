49e3bd307e539099af94ce2033b1757d2933fcd9
MaxLimo <>
2022-04-09 18:48:38 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+print("Welcome to supplyManage by Cosmos ./Code\nType in .{help} to get started with commands")
+
+initcmd = input("./: ")
+
+if initcmd == ".{help}":
+    print(
+        "Welcome to supplyManage\n"
+        "Start by creating chain: ./create{}\n"
+        "Add product to chain by ./chain/pro\n"
+        "Add chain type by: ./chain/type\n"
+        "Add destination by: ./chain/desti\n"
+        "Right now, only one chain works. Multiple can be added, but system will fail"
+        "Type ./chain/log for all lists printed\n"
+        "Now rerun program to load\n"
+    )
+while True:
+  if initcmd == "./create{}":
+    ask = input("Name of Chain: ")
+    chain = ask
+    chainpro = [] # List of products 
+    chaintype = [] # List of Types
+    destichain = [] # final place of products
+    print("Chain {} is made.".format(chain))
+    initcmd = input("./: ")
+  
+  elif initcmd == "./{}/pro".format(chain):
+    productname = input("Name of Product: ")
+    chainpro.append(productname)
+    initcmd = input("./: ")
+  
+  elif initcmd == "./{}/type".format(chain):
+    typepro = input("Shipping Type: ")
+    chaintype.append(chaintype)
+    initcmd = input("./: ")
+  
+  elif initcmd == "./{}/desti".format(chain):
+    prodesti = input("Final Place of Product: ")
+    destichain.append(prodesti)
+    initcmd = input("./: ")
+  
+  elif initcmd == "./{}/log".format(chain):
+    asklogprint = input("We are going to export as a txt file. y or n")
+    if asklogprint == "n":
+      initcmd = input("./: ")
+    elif asklogprint == 'y':
+      import json
+      with open("DataProducts.txt", 'w') as savepro:
+        savepro.write(json.dumps(chainpro))
+        savepro.write(json.dumps(chaintype))
+        savepro.write(json.dumps(destichain))
+      initcmd = input("./: ")
+    else:
+      print("Invalid Response")
+      initcmd = input("./: ")
+  else:
+    print("Invalid Command")
